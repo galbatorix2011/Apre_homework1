1bf0d6da195461ffb7d839a536b60fb83d1ba97b
sdrrv <duartecelvas@gmail.com>
2021-10-14 14:58:10 +0100

    Inicial Commit

---
+++ hw01.py
@@ -0,0 +1,64 @@
+"""
+###################
+Group 132
+###################
+
+Afonso Almeida 92790
+Duarte Elvas 98564
+
+"""
+# ---------------------------------Imports------------------------------
+
+import matplotlib.pyplot as plt
+
+# --------------------------------Functions-----------------------------
+
+def getVectors(indice):
+    res = []
+    for line in traningData:
+        res.append(line[indice])
+    return res
+
+def getTrainingData(fileName):
+    res = []
+    #Reads the file
+    with open(fileName) as f: 
+        lines = f.readlines()
+    #Formats all lines
+    for line in lines:
+        tmp = line[:-1].split(",") #removes the /n from the end of each line
+        for i in range(len(tmp) - 1):
+            try: #Truns every str into int
+                tmp[i] = int(tmp[i])
+                res.append(tmp)
+            except Exception as e:
+                pass #ignores lines that have an '?'
+    return res
+
+# ------------------------------Global-Variables---------------------------
+
+traningData = getTrainingData("almeida.txt") #Training Data Stored
+
+#Atrributes
+titles = ["Clump Thickness", "Cell Size Uniformity", "Cell Shape Uniformity", "Marginal Adhesion", "Single Epi Cell Size", \
+"Bare Nuclei", "Bland Chromatin", "Normal Nucleoli", "Mitoses"] 
+
+#---------------------------------------------------------------------------
+
+fig, _ = plt.subplots(nrows=3, ncols=3, figsize=(10,7)) # creating the plots
+fig.tight_layout(pad=4.0) # spacing
+ast=fig.axes # getting a list with the axes
+fig.canvas.set_window_title('AP HW01 G132') # Window Title
+
+
+#Setting the titles
+for i in range(len(ast)):
+
+    vectors = getVectors(i)
+    ast[i].title.set_text(titles[i])
+    ast[i].set_ylabel("Amount")
+    ast[i].set_xlabel("Value")
+    ast[i].hist(vectors, 4, density = True, edgecolor='red')
+
+
+plt.show()
