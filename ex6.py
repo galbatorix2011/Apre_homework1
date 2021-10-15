5cfa9ef3fa494116ecf17f5366b89068a9b511cf
galbatorix2011 <afonsoalmeida.b@gmail.com>
2021-10-15 09:29:35 +0100

    break point added

--- ex6.py
+++ ex6.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 # --------------------------------Functions-----------------------------
 
+
 def getVectors(indice):
     res = []
     for line in data:
@@ -23,27 +24,30 @@ def getVectors(indice):
 
 def getData(fileName):
     res = []
-    #Reads the file
-    with open(fileName) as f: 
+    # Reads the file
+    with open(fileName) as f:
         lines = f.readlines()
-    #Formats all lines
+    # Formats all lines
     for line in lines:
         tmp = line[:-1].split(",")
         if '?' in line:
             continue
-        res.append([int(tmp[i]) if i < len(tmp) - 1 else tmp[i] for i in range(len(tmp))])
+        res.append([int(tmp[i]) if i < len(tmp) - 1 else tmp[i]
+                    for i in range(len(tmp))])
     return res
 
 # ------------------------------Global-Variables---------------------------
- 
+
+
 def getDistance(point1, point2):
     sum = 0
     for i in range(0, len(point1) - 1):
         sum += (point1[i] - point2[i])**2
     return math.sqrt(sum)
 
+
 def getKnn(point1, indexes):
-    nns = [[-1, -1] for i in range(k)] # -1 when there is nothing there
+    nns = [[-1, -1] for i in range(k)]  # -1 when there is nothing there
     points = [data[i] for i in indexes]
     empty = k
     for point2 in points:
@@ -60,12 +64,14 @@ def getKnn(point1, indexes):
                 if dist < nns[i][0]:
                     nns[i][0] = dist
                     nns[i][1] = point2
+                    break
     return nns
+
 
 def getClassification(point, indexes):
     nns = getKnn(point, indexes)
     neighbours = [element[1][-1] for element in nns]
-    numMalignant = 0 
+    numMalignant = 0
     numBenign = 0
     for neighbour in neighbours:
         if neighbour == 'malignant':
@@ -75,32 +81,43 @@ def getClassification(point, indexes):
     return 'malignant' if numMalignant > numBenign else 'benign'
 
 
-data = getData("TrainingData.txt") #Training Data Stored
+data = getData("TrainingData.txt")  # Training Data Stored
 k = int(input('k: '))
 
-kf = KFold(n_splits = 10, random_state = 130, shuffle = True)
+kf = KFold(n_splits=10, random_state=130, shuffle=True)
 numRightClassifications = 0
 numClassifications = len(data)
 accuracies = []
 
 
-for train_index , test_index in kf.split(data):
-    print(test_index)
+for train_index, test_index in kf.split(data):
+    # print(test_index)
     ola = 0
     oi = 0
     for index in test_index:
-        point = data[index];
+        point = data[index]
         pointClass = point[-1]
         ola += 1
         if getClassification(point, train_index) == pointClass:
             numRightClassifications += 1
             oi += 1
-        accuracies.append(oi / ola)
+    accuracies.append(oi / ola)
 
 sum = 0
 for value in accuracies:
     sum += value
-print('Accuracy1 = ' + str(sum / len(accuracies)))
-print('Accuracy2 = ' + str(numRightClassifications / numClassifications))
-    
+print('Accuracy1 testes = ' + str(sum / len(accuracies)))
+print('Accuracy2 testes = ' + str(numRightClassifications / numClassifications))
 
+ola = 0
+oi = 0
+indexes = [i for i in range(len(data))]
+accuracies = []
+for point in data:
+    pointClass = point[-1]
+    ola += 1
+    if getClassification(point, indexes) == pointClass:
+        oi += 1
+
+
+print('Accuracy1 treino = ' + str(oi / ola))
