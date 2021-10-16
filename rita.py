5f8b84727de1195a5dc0039c9380bb0c0ad7c5f6
sdrrv <duartecelvas@gmail.com>
2021-10-16 11:33:51 +0100

    Rita populated

--- rita.py
+++ rita.py
@@ -2,7 +2,34 @@ import numpy as np
 from scipy.stats import norm
 from scipy.stats import multivariate_normal
 
+class data:
+    def __init__(self, y1, y2, y3, y4, classer):
+        self.y1 = y1
+        self.y2 = y2
+        self.y3 = y3
+        self.y4 = y4
+        self.classer = classer
+
+
+def populate():
+    res = []
+    res.append(data(0.6, 'A', 0.2, 0.4, 0)) #x1
+    res.append(data(0.1, 'B', -0.1, -0.4, 0)) #x2
+    res.append(data(0.2, 'A', -0.1, 0.2, 0)) #x3 
+    res.append(data(0.1, 'C', 0.8, 0.8, 0)) #x4
+
+    res.append(data(0.3, 'B', 0.1, 0.3, 1)) #x5
+    res.append(data(-0.1, 'C', 0.2, -0.2, 1)) #x6
+    res.append(data(0.3, 'C', -0.1, 0.2, 1)) #x7
+    res.append(data(0.2, 'B', 0.5, 0.6, 1)) #x8
+    res.append(data(0.4, 'A', -0.4, -0.7, 1)) #x9
+    res.append(data(-0.2, 'C', 0.4, 0.3, 1)) #x10
+
+    return res
+
 def main():
+    dataArray = populate()
+    
     c = eval(input("Classe: "))
 
     pC = 0.4 if c == 0 else 0.6
@@ -25,9 +52,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-    #var = multivariate_normal(mean = [80, 203.3333], cov = [[100, 50], [50, 233.3333]])
-    #print(var.pdf([100, 225]))
-
      
 
 
