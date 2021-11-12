adbf1488c1ebb7495c9e76aeae7143d638cc2675
sdrrv <duartecelvas@gmail.com>
2021-11-12 14:51:27 +0000

    Pretty Version

--- HW04/ex04.py
+++ HW04/ex04.py
@@ -1,6 +1,3 @@
-from sklearn.model_selection import KFold
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 import matplotlib.pyplot as plt
@@ -29,41 +26,8 @@ def divide(data):  # will divide the data in order to be processed by the Multin
         X.append(line[:-1])
         y.append(line[-1])
     return (X, y)
-
-
-#def outBinarization(dataOut):
-#    return [lambda(i : 1 if i == "malignant" else 0) for i in dataOut]
-
-def getAccuracy(predictions, real):
-    return sum([1 if predictions[i] == real[i] else 0 for i in range(
-            len(predictions))]) / len(predictions) 
-
-def getMeanAccuracy(accuracies):
-    return sum(accuracies) / len(accuracies)
-
-def getECRScore(dataOut, prediction, n):
-    c0Benign = 0
-    c0Malignant = 0
     
-    c1Benign = 0
-    c1Malignant = 0
-    for i in range(len(prediction)):
-        if prediction[i] == 0:
-            if dataOut[i] == "malignant":
-                c0Malignant +=1
-            else:
-                c0Benign +=1
-        else:
-            if dataOut[i] == "malignant":
-                c1Malignant +=1
-            else:
-                c1Benign +=1
-    c0Total = c0Benign + c0Malignant
-    print("c0: " + str(c0Total) )
-    c1Total = c1Benign + c1Malignant
-    return ((c0Total - max(c0Benign, c0Malignant)) + (c1Total - max(c1Benign, c1Malignant))) / n
-    
-def getECRScore(dataOut, prediction, n):
+def getECRScore(dataOut, labelK2, n): #Will calcuçate the ECR score with n dimentions
     c0Benign = 0
     c0Malignant = 0
     
@@ -72,18 +36,18 @@ def getECRScore(dataOut, prediction, n):
     
     c2Benign = 0
     c2Malignant = 0
-    for i in range(len(prediction)):
-        if prediction[i] == 0:
+    for i in range(len(labelK2)):
+        if labelK2[i] == 0:
             if dataOut[i] == "malignant":
                 c0Malignant +=1
             else:
                 c0Benign +=1
-        elif prediction[i] == 1:
+        elif labelK2[i] == 1:
             if dataOut[i] == "malignant":
                 c1Malignant +=1
             else:
                 c1Benign +=1
-        elif prediction[i] == 2:
+        elif labelK2[i] == 2:
             if dataOut[i] == "malignant":
                 c2Malignant +=1
             else:
@@ -95,61 +59,45 @@ def getECRScore(dataOut, prediction, n):
     return ((c0Total - max(c0Benign, c0Malignant)) + (c1Total - max(c1Benign, c1Malignant)) + (c2Total - max(c2Benign, c2Malignant))) / n
     
 data = getData("data.txt")  # Training Data Stored
-
 dataIn, dataOut = divide(data)
 
 newData = SelectKBest(mutual_info_classif, k=2).fit_transform(dataIn,dataOut)
 
-
 kmeans = KMeans(n_clusters=2, random_state=0).fit(dataIn)
 kmeansK3s = KMeans(n_clusters=3, random_state=0).fit(dataIn)
-
-
 kmeansK3 = KMeans(n_clusters=3, random_state=0).fit(newData)
 
-prediction = list(kmeans.labels_)
-predictionK3s = list(kmeansK3s.labels_)
-prediction2 = list(kmeansK3.labels_)
-print("ECR K2 ---> "+ str(getECRScore(dataOut, prediction, 2)))
-print("ECR K3 ---> "+ str(getECRScore(dataOut, predictionK3s, 3)))
-print()
-print("Silhouette K2 ---> "+ str(silhouette_score(dataIn, prediction)))
-print("Silhouette K3 ---> "+ str(silhouette_score(dataIn, predictionK3s)))
+labelK2 = list(kmeans.labels_)
+labelK3 = list(kmeansK3s.labels_)
+labelEx5 = list(kmeansK3.labels_)
+
+print("ECR K2 ---> "+ str(getECRScore(dataOut, labelK2, 2)))
+print("ECR K3 ---> "+ str(getECRScore(dataOut, labelK3, 3)))
+print("")
+print("Silhouette K2 ---> "+ str(silhouette_score(dataIn, labelK2)))
+print("Silhouette K3 ---> "+ str(silhouette_score(dataIn, labelK3)))
 
 print("#-" * 30)
 
-print("ECR 5 ---> "+ str(getECRScore(dataOut, prediction2, 3)))
-print("Silhouette 5 ---> "+ str(silhouette_score(newData, prediction2)))
-
-      
-
-
+print("ECR 5 ---> "+ str(getECRScore(dataOut, labelEx5, 3)))
+print("Silhouette 5 ---> "+ str(silhouette_score(newData, labelEx5)))
 
 cluster0 = []
 cluster1 = []
 cluster2 = []
 
-
-for i in range(len(prediction2)):
-    if prediction2[i] == 0 :
+for i in range(len(labelEx5)): # Will divide the data into clusters
+    if labelEx5[i] == 0 :
         cluster0.append(newData[i])
-    elif prediction2[i] == 1:
+    elif labelEx5[i] == 1:
         cluster1.append(newData[i])
     else:
         cluster2.append(newData[i])
 
 plt.scatter([i[0] for i in cluster0] , [i[1] for i in cluster0], color = "blue", label = "cluster 0")
-
 plt.scatter([i[0] for i in cluster1] , [i[1] for i in cluster1], color = "green", label = "cluster 1")
-
 plt.scatter([i[0] for i in cluster2] , [i[1] for i in cluster2], color = "red", label = "cluster 2")
 
-#plt.scatter([point[0] for point in newData], [point[1] for point in newData])
-
-
-#plt.scatter([newData[i][0]  for i in range(len(prediction)) if prediction[i] == 1 ],[newData[i][1]  for i in range(len(prediction)) if prediction[i] == 1 ])
-#plt.scatter([newData[i][0]  for i in range(len(prediction)) if prediction[i] == 0 ],[newData[i][1]  for i in range(len(prediction)) if prediction[i] == 0 ])
 plt.legend()
 plt.show()
-#print(x)
     
